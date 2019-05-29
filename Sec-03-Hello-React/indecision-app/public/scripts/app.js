6894bc895bfa1b65099e160336327dabc4d91fa7
Jake Huneycutt <hjhuney@gmail.com>
2019-05-29 00:04:44 -0400

    updates

--- Sec-03-Hello-React/indecision-app/public/scripts/app.js
+++ Sec-03-Hello-React/indecision-app/public/scripts/app.js
@@ -1,14 +1,118 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running!');
 
+// create an app object title / subtitle
+// user title / subtitle in the template
+// render template
+
 // JSX - JavaScript XML
 
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.js!'
+var appInfo = {
+    title: "Merfin Melvin's Merfin' App!",
+    subtitle: "Let's merf this hizny up, booooyyyzzzz!",
+    userTitle: "Stuff"
+
+    // let template = 
+    //     <div>
+    //     <h1>Indecision App</h1> 
+    //     <p>This is some info!</p>
+    //     <ol>
+    //     <li>Item One</li>
+    //     <li>Item One</li>
+    //     </ol>
+    //     </div>;
+
+};var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        appInfo.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        appInfo.subtitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        )
+    )
 );
+
+// user object
+
+var user = {
+    name: 'Walrus McConnell',
+    age: 59,
+    location: 'Lubbock, TX'
+};
+
+// user variables
+
+var userName = 'Merfin Melvin';
+var userAge = 48;
+var userLocation = "Albuquerque, NM";
+
+// create a new templete with JSX expression
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        userName.toUpperCase()
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        userAge
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        userLocation
+    )
+);
+
+// create template with object
+
+var template3 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- Sec-03-Hello-React/indecision-app/src/app.js
+++ Sec-03-Hello-React/indecision-app/src/app.js
@@ -1,8 +1,69 @@
 console.log('App.js is running!');
 
+// create an app object title / subtitle
+// user title / subtitle in the template
+// render template
+
 // JSX - JavaScript XML
 
-let template = <p>This is JSX from app.js!</p>;
+let appInfo = {
+    title: "Merfin Melvin's Merfin' App!", 
+    subtitle: "Let's merf this hizny up, booooyyyzzzz!", 
+    userTitle: "Stuff"
+}
+
+// let template = 
+//     <div>
+//     <h1>Indecision App</h1> 
+//     <p>This is some info!</p>
+//     <ol>
+//     <li>Item One</li>
+//     <li>Item One</li>
+//     </ol>
+//     </div>;
+
+let template = 
+    <div>
+    <h1>{appInfo.title}</h1> 
+    <p>{appInfo.subtitle}</p>
+    <ol>
+    <li>Item One</li>
+    <li>Item One</li>
+    </ol>
+    </div>;
+
+
+// user object
+
+let user = {
+    name: 'Walrus McConnell',
+    age: 59, 
+    location: 'Lubbock, TX'
+};
+
+// user variables
+
+let userName = 'Merfin Melvin';
+let userAge = 48;
+let userLocation = "Albuquerque, NM"
+
+// create a new templete with JSX expression
+let template2 = 
+    <div>
+    <h1>{userName.toUpperCase()}</h1> 
+    <p>Age: {userAge}</p>
+    <p>Location: {userLocation}</p>
+    </div>;
+
+// create template with object
+
+let template3 = 
+    <div>
+    <h1>{user.name}</h1> 
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+    </div>;
+
 let appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
